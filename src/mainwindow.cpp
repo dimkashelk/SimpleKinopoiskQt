379af4cc83703f507688640950880d8699f6d744
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2021-12-07 18:22:00 +0300

    Add StackedWidget

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,7 +2,12 @@
 #include "ui_mainwindow.h"
 #include "newscard.h"
 #include <Qt>
+#include <QSqlDatabase>
+#include <QImage>
 #include <QAction>
+#include <QSqlQuery>
+#include <QBuffer>
+#include <QSqlError>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -10,11 +15,27 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    this->ui->popular_scroll->addWidget(new NewsCard(this));
-    this->ui->popular_scroll->addWidget(new NewsCard(this));
-    this->ui->popular_scroll->addWidget(new NewsCard(this));
-    this->ui->popular_scroll->addWidget(new NewsCard(this));
-    this->ui->popular_scroll->addWidget(new NewsCard(this));
+    db = QSqlDatabase::addDatabase("QSQLITE", "db");
+    db.setDatabaseName("db.db");
+    db.open();
+
+    QPixmap image("image.jpg");
+    QByteArray inByteArray;
+    QBuffer inBuffer(&inByteArray);
+    inBuffer.open(QIODevice::WriteOnly);
+    image.save(&inBuffer, "JPG");
+
+    QSqlQuery query = QSqlQuery(db);
+    query.prepare("UPDATE news SET image = :imageData WHERE id = 1");
+    query.bindValue(":imageData", inByteArray);
+    query.exec();
+
+    QSqlQuery test("SELECT image, title, description FROM news WHERE id = 1", db);
+    test.next();
+    QImage t;
+    t.loadFromData(test.value(0).toByteArray());
+    t = t.scaledToWidth(250);
+    this->ui->popular_scroll->addWidget(new NewsCard(this, test.value(1).toString(), test.value(2).toString(), t));
 }
 
 MainWindow::~MainWindow()
