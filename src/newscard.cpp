9b483e1b0e0221c50fb029e42d5255af4eb6d86d
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2021-12-07 19:47:52 +0300

    Change QLabel to ClickableQLabel

--- src/newscard.cpp
+++ src/newscard.cpp
@@ -1,5 +1,6 @@
 #include "newscard.h"
 #include "ui_newscard.h"
+#include "clickedqlabel.h"
 
 NewsCard::NewsCard(QWidget *parent) :
     QFrame(parent),
@@ -15,13 +16,26 @@ NewsCard::NewsCard(QWidget *parent, QString title, QString text, QImage image) :
     ui(new Ui::NewsCard)
 {
     ui->setupUi(this);
+    original_image = image;
 
     this->setMinimumWidth(150);
     this->setMaximumHeight(250);
 
+    delete this->ui->news_label;
+    delete this->ui->news_text;
+    delete this->ui->news_image;
+
+    ClickableQLabel *news_label = new ClickableQLabel("text", this);
+    this->ui->verticalLayout->addWidget(news_label);
+
+    ClickableQLabel *news_image = new ClickableQLabel("text", this);
+    this->ui->horizontalLayout->addWidget(news_image);
+
+
+
     this->ui->news_label->setText(title);
 
-    this->ui->news_text->setText(text);
+    //this->ui->news_text->setText(text);
 
     this->ui->news_image->setPixmap(QPixmap::fromImage(image, Qt::AutoColor));
 }
