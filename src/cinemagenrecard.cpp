e5e4a1c0226645cba2b5d9c39da8218f1f35d2bd
dimkashelk <dmitrii.shelkovnikov@yandex.ru>
2021-12-10 20:36:55 +0300

    Update CinemaGenreCard

--- src/cinemagenrecard.cpp
+++ src/cinemagenrecard.cpp
@@ -1,5 +1,6 @@
 #include "cinemagenrecard.h"
 #include "ui_cinemagenrecard.h"
+#include <QSqlQuery>
 
 CinemaGenreCard::CinemaGenreCard(QWidget *parent) :
     QFrame(parent),
@@ -11,4 +12,35 @@ CinemaGenreCard::CinemaGenreCard(QWidget *parent) :
 CinemaGenreCard::~CinemaGenreCard()
 {
     delete ui;
+}
+
+
+CinemaGenreCard::CinemaGenreCard(QWidget *parent, QString id_genre) :
+    QFrame(parent),
+    ui(new Ui::CinemaGenreCard)
+{
+    ui->setupUi(this);
+
+    db = QSqlDatabase::addDatabase("QSQLITE", "db");
+    db.setDatabaseName("db.db");
+    db.open();
+
+    QSqlQuery cinema_query;
+    cinema_query.prepare("SELECT image FROM cinema WHERE genre = (:genreId)");
+    cinema_query.bindValue(":genreId", id_genre);
+    cinema_query.exec();
+
+    QList<QString> cinemas;
+    if (cinema_query.isActive()) {
+        while (cinema_query.next()) {
+            cinemas.append(cinema_query.value(0).toString());
+        }
+    }
+
+    QSet<QString> five_cinema;
+    while (five_cinema.size() < 5) {
+        five_cinema.insert(cinemas[rand() % cinemas.size()]);
+    }
+
+
 }
--- src/cinemagenrecard.h
+++ src/cinemagenrecard.h
@@ -2,6 +2,7 @@
 #define CINEMAGENRECARD_H
 
 #include <QFrame>
+#include <QSqlDatabase>
 
 namespace Ui {
 class CinemaGenreCard;
@@ -12,11 +13,17 @@ class CinemaGenreCard : public QFrame
     Q_OBJECT
 
 public:
+
     explicit CinemaGenreCard(QWidget *parent = nullptr);
+
+    CinemaGenreCard(QWidget *parent = nullptr, QString id_genre = "1");
+
     ~CinemaGenreCard();
 
 private:
     Ui::CinemaGenreCard *ui;
+
+    QSqlDatabase db;
 };
 
 #endif // CINEMAGENRECARD_H
